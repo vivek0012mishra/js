38738fc7c1a88ac20ff656aeea40136a9bc6a4fa
Vivek Kumar Mishra <111078673+vivek0012mishra@users.noreply.github.com>
2024-01-01 19:40:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js node as newer version
+// alert(3+3)//we are using nodejs not browser
+
+console.log(3
+    +
+    3)//code readibility should be high
+
+
+    let name="pandit"
+    let age=23
+    let isLoggedIn = false
+
+    //number=>2 to power 53
+    //bigint
+    //string=>""
+    //boolean=>true/false
+    //null=>standalone value
+    //undefined =>
+    // symbol =>unique
+
+
+    //object
+
+    console.log(typeof"vivek");
+    console.log(typeof undefined);//undefined
+    console.log(typeof null);//object
