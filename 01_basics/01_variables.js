6adb9eeace8a33d4843cdc69aae01606f3b11277
Vivek Kumar Mishra <111078673+vivek0012mishra@users.noreply.github.com>
2024-01-01 19:01:00 +0000

    Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId =12342
+let accountEmail ="vivekkrmishra7255@gmail.com"
+var accountPassword ="12345"
+accountCity ="kolkata"
+let accountState;
+
+//accountId =4//not allowed
+accountEmail ="rahul01@gmail.com"
+accpuntPassword ="54321"
+accountCity ="mumbai"
+
+console.log(accountId);
+
+/*
+prefer not using var
+because of issue in block scope and functionl scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
